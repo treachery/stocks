87e0f10cb519c568c463897c21b7a555c3e48243
lancerchen <lancerchen@tencent.com>
2025-03-20 16:43:31 +0800

    fix

--- Stock_Bond_Balancing.py
+++ Stock_Bond_Balancing.py
@@ -3,7 +3,6 @@ import xmltodict
 import requests
 # 对比中美国债/中美大盘指数的投资性价比
 
-
 # 国债 https://data.eastmoney.com/cjsj/zmgzsyl.html
 def get_cn_bonds():
     requests.get("https://iftp.chinamoney.com.cn/chinese/sddsintigy/")
@@ -23,7 +22,6 @@ def get_cn_bonds():
     oneRate = str(round(float(record['oneRate']), 2))+'%'
     tenRate = str(round(float(record['tenRate']), 2))+'%'
     print('中国1年期国债利率:', oneRate, '中国10年期国债利率:', tenRate)
-
 
 def get_us_bonds():
     us_bonds_uri = "https://home.treasury.gov/resource-center/data-chart-center/interest-rates/pages/xml?data=daily_treasury_yield_curve&field_tdr_date_value=2024"
@@ -47,10 +45,10 @@ def get_index(max_rate: float):
     if reply.status_code != 200:
         raise Exception("get index_uri status_code:", reply.status_code)
     # 'CSIH30533', 'SH000852', 'HKHSI', 'SH000905'
-    index_codes = set(['SH000300', 'SP500', 'NDX', 'HKHSTECH', 'CSIH30533'])
+    index_codes = set(['SH000300', 'SP500', 'NDX', 'HKHSTECH', 'HKHSCEI', 'SZ399989', 'SZ399997','CSIH30533'])
 
     rate_10y = (max_rate/100+1)**10
-    print('当前国债收益率:', max_rate, ', 国债10年复利:', round(rate_10y, 3))
+    # print('当前国债收益率:', max_rate, ', 国债10年复利:', round(rate_10y, 3))
     for i in reply.json()['data']['items']:
         if i['index_code'] in index_codes:
             # print(i)
@@ -62,33 +60,35 @@ def get_index(max_rate: float):
             # ROE虽然好，但要警惕苹果公司这种靠金融操作、债务杠杆推高收益率的公司
             _suggested_position = 0
             rate_per = (max_rate/100)
-            if i['roe'] > rate_per and i['roe'] <= 0.3:
-                roe_x = 1 + ((i['roe'] - (3*rate_per)) / (i['roe'] + 3*rate_per)) # 认为roe > 3倍无风险利率是分界，高于赚的是easy money，享受额外加成
+            if i['roe'] > rate_per:
+                # roe_x = 1 + ((i['roe'] / (3*rate_per)) / (i['roe'] + 3*rate_per)) # 认为roe > 3倍无风险利率是分界，高于赚的是easy money，享受额外加成
                 # pe_h = (0.5 + float(i['pe_over_history'])) # pe百分位低于50%则加成更高
-                pg_x = i['pe']/(i['roe']*100) # 市赚率, 市赚率大于1高估，小于1低估
-                rate_x = rate_per + 1
-                eg_x = 1/i['pe']/pg_x # 预期收益率
-                if eg_x > 3*rate_per: # 满仓
-                    sp_x = 1
-                elif eg_x > rate_per:
-                    sp_x = 0.5 + (eg_x - rate_per) / (4 * rate_per)
-                elif eg_x < rate_per/3: # 空仓
-                    sp_x = 0
-                else:
-                    sp_x = 0.5 + (eg_x-rate_per)*3 / (4 * rate_per)
+                # pg_x = i['pe']/(i['roe']*100) # 市赚率, 市赚率大于1高估，小于1低估
+                # rate_x = rate_per + 1
+                # eg_x = 1/i['pe']/pg_x # 预期收益率
+                # if eg_x > 3*rate_per: # 满仓
+                #     sp_x = 1
+                # elif eg_x > rate_per:
+                #     sp_x = 0.5 + (eg_x - rate_per) / (4 * rate_per)
+                # elif eg_x <= rate_per/2: # 空仓
+                #     sp_x = 0
+                # else:
+                #     sp_x = 0.5 + (eg_x-rate_per)*3 / (4 * rate_per)
+                roe_pb_x = round(((i['roe'] - rate_per) / (3*rate_per)) * i['roe'] / i['pb'], 4)
 
-                _suggested_position = sp_x * roe_x #* pe_h
+                # _suggested_position = (roe_pb_x - rate_per) / (roe_pb_x + rate_per)  #* pe_h
 
                 # print('roe_x:', roe_x, ', pe_h:', 0, ', pg_x:', pg_x, ', rate_x:', rate_x, ', eg_x:', eg_x, ' ,sp_x:', sp_x)
-            if _suggested_position > 0:
+            # if _suggested_position > 0:
+                # print(i)
+            # if i['pe'] < 20 and i['pb'] < 1:
                 print(i['index_code'], i['name'], ': PE:', round(i['pe'], 3), ', PB:', round(i['pb'], 3), ', ROE:', "{:.2%}".format(round(i['roe'], 4)),
-                    ', PE百分位:', pe_per, ', PB百分位:', pb_per, ', 股息率:', "{:.2%}".format(i['yeild']), ', ROE/PB:', "{:.2%}".format(roe_pb),
-                    ', 建议仓位:', "{:.1%}".format(_suggested_position))
-
+                      ', PE百分位:', pe_per, ', PB百分位:', pb_per, ', 股息率:', "{:.2%}".format(i['yeild']),
+                      ', ROE/PB:', "{:.2%}".format(roe_pb), ', 预期收益率(修正ROE/PB):', "{:.1%}".format(roe_pb_x))
 
 date = time.strftime('%Y-%m-%d', time.localtime(time.time()))
 response = requests.get('https://api.exchangerate-api.com/v4/latest/USD')
-print('\n')
+print('计算仓位时，认为roe > 3倍无风险利率是分界，高于赚的是easy money，享受额外加成\n')
 print('DATE:', date, '汇率(USD/CNY):', response.json()['rates']['CNY'])
 
 # get_cn_bonds()
--- UnderValue.py
+++ UnderValue.py
@@ -1,8 +1,7 @@
 import requests
 
-
 # 现金流折现率估值法
-def DCF(cash, grow_year, grow_rate, discount):
+def DCF(cash, grow_year, grow_rate, permanent_rate, discount):
     cash_rate = 1 + ((grow_rate - discount) / 100)
 
     total_cash_rate = 0
@@ -13,23 +12,28 @@ def DCF(cash, grow_year, grow_rate, discount):
         max_cash_rate_year = cash_rate ** i
 
     # 永续现金流
-    for i in range(20):  # 永续增长
-        total_cash_rate += max_cash_rate_year * (1 - (discount / 100)) ** i
+    for i in range(50):  # 永续增长
+        total_cash_rate += max_cash_rate_year * (1 + (permanent_rate / 100)) * (1 - (discount / 100)) ** i
 
     return cash * total_cash_rate
 
-
 # 投资回报率(按芒格说法，按PB=1买入一个公司，则投资回报率等于净资产收益率)
-def ROIC(roic, pb, grow):
-    return roic / pb * (1 + (grow / 100))
+def ROIC(roic=0.0, pb=1000, grow=-100) -> float:
+    result = 0.0
+    try:
+        result = roic / pb * (1 + (grow / 100))
+    except TypeError:
+        # print('ROIC, ', ValueError)
+        pass
+    return result
 
 # 以DCF和ROIC筛选出优质且低估的企业
 # 选股条件
-# 市净率MRQ为0~10；
+# 市净率<10；
 # 总市值为>100亿；
 # 每股经营现金流为>1：
-# 投入资本回报率ROIC为>10%；
-# 净利率为20%~99%；
+# 投入资本回报率ROIC为>5%；
+# 净利率为>10%；
 # 净利润3年复合增长率为>10%；
 # 营收3年复合增长率为>10%；
 # 预测净利润同比增长为>10%；
@@ -38,13 +42,14 @@ def ROIC(roic, pb, grow):
 
 # (PBNEWMRQ>0)(PBNEWMRQ<=10)(TOTAL_MARKET_CAP>10000000000)(ROIC>10)(SALE_NPR>10)(NETPROFIT_GROWTHRATE_3Y>10)(INCOME_GROWTHRATE_3Y>10)(PREDICT_NETPROFIT_RATIO>10)(PREDICT_INCOME_RATIO>10)(PER_NETCASH_OPERATE>1)
 # (PBNEWMRQ>0)(PBNEWMRQ<=10)(TOTAL_MARKET_CAP>1000000000)(ROIC>5)(SALE_NPR>10)(NETPROFIT_GROWTHRATE_3Y>0)(INCOME_GROWTHRATE_3Y>0)(PREDICT_NETPROFIT_RATIO>0)(PREDICT_INCOME_RATIO>0)(PER_NETCASH_OPERATE>1)
+# (PBNEWMRQ<=10)(PE9<40)(TOTAL_MARKET_CAP>10000000000)(ROIC>5)(SALE_NPR>10)(NETPROFIT_GROWTHRATE_3Y>-5)(INCOME_GROWTHRATE_3Y>-5)(PREDICT_NETPROFIT_RATIO>0)(PREDICT_INCOME_RATIO>0)(PER_NETCASH_OPERATE>1)
 uri = "https://datacenter.eastmoney.com/stock/selection/api/data/get/"
 d = {
     "type": "RPTA_SECURITY_STOCKSELECT",
-    "sty": "ECURITY_CODE,SECURITY_NAME_ABBR,NEW_PRICE,PBNEWMRQ,ROIC,NETPROFIT_GROWTHRATE_3Y,INCOME_GROWTHRATE_3Y,PREDICT_NETPROFIT_RATIO,PREDICT_INCOME_RATIO,SALE_NPR,PER_NETCASH_OPERATE,TOTAL_MARKET_CAP,PE9",
-    "filter": '(MARKET IN ("上交所主板","深交所主板","深交所创业板","上交所科创板","上交所风险警示板","深交所风险警示板","北京证券交易所"))(PBNEWMRQ>0)(PBNEWMRQ<=10)(PE9<40)(TOTAL_MARKET_CAP>1000000000)(ROIC>5)(SALE_NPR>10)(NETPROFIT_GROWTHRATE_3Y>-5)(INCOME_GROWTHRATE_3Y>-5)(PREDICT_NETPROFIT_RATIO>0)(PREDICT_INCOME_RATIO>0)(PER_NETCASH_OPERATE>1)',
+    "sty": "ECURITY_CODE,SECURITY_NAME_ABBR,NEW_PRICE,PBNEWMRQ,ROIC,NETPROFIT_GROWTHRATE_3Y,INCOME_GROWTHRATE_3Y,PREDICT_NETPROFIT_RATIO,PREDICT_INCOME_RATIO,SALE_NPR,PER_NETCASH_OPERATE,TOTAL_MARKET_CAP,PE9,TOTAL_MARKET_CAP,ROIC",
+    "filter": '(MARKET IN ("上交所主板","深交所主板","深交所创业板","上交所科创板","上交所风险警示板","深交所风险警示板","北京证券交易所"))(PBNEWMRQ<=10)(PE9>0)(PE9<50)(TOTAL_MARKET_CAP>10000000000)',
     "p": 1,
-    "ps": 500,
+    "ps": 5000,
     "sr": -1,
     "st": "TOTAL_MARKET_CAP",
     "source": "SELECT_SECURITIES",
@@ -56,27 +61,46 @@ if r.status_code != 200:
 
 data = r.json()["result"]["data"]
 
-print(['NAME', 'NEW_PRICE', 'PB', 'PE', 'ROIC', 'GROW', 'ROIC_RETURN', 'ROIC_PRICE', 'DCF_PRICE', 'SCORE', '市赚率'])
+Million = 1000*1000
+Billion = 1000*Million
+print('datalen: ', len(r.json()["result"]["data"]))
+# print(['NAME', 'NEW_PRICE', 'PB', 'PE', 'ROIC', 'GROW', 'ROIC_RETURN', 'ROIC_PRICE', 'DCF_PRICE', 'SCORE', '市赚率'])
+print(['NAME', 'NEW_PRICE', 'PB', 'PE', 'GROW', 'DCF_PRICE', 'SCORE', 'ROIC'])
 for i in r.json()["result"]["data"]:
-    # ROIC
-    # 非常非常保守的增长率估算: MIN(过去3年平均增长率的一半,分析师预估未来的增长率的1/3)
-    GROWTHRATE = min(i['NETPROFIT_GROWTHRATE_3Y'] / 2, i['INCOME_GROWTHRATE_3Y'] / 2, i['PREDICT_NETPROFIT_RATIO'] / 3,
-                     i['PREDICT_INCOME_RATIO'] / 3)
-    ROIC_RETURN = ROIC(i['ROIC'], i['PBNEWMRQ'], GROWTHRATE)
-    # 以长期投资回报率5%为分界, 计算合理估值
-    ROIC_PRICE = ROIC_RETURN / 5 * i['NEW_PRICE']
+    try:
+        # ROIC
+        # 非常非常保守的增长率估算: MIN(过去3年平均增长率的1/3,分析师预估未来的增长率的1/2)
+        GROWTHRATE = min(i['NETPROFIT_GROWTHRATE_3Y']*1.0 / 2.0, i['INCOME_GROWTHRATE_3Y']*1 / 3)
 
-    # DCF
-    # 以 净利润/ROIC/当前规模 判断公司的增长潜力, 高利润率，市值1000亿以下企业认为增长更久
-    CASH_PER = 1 + ((GROWTHRATE - 10) / 100)
-    GROW_YEAR = 5  # 利润率低，增长5年
-    if i['SALE_NPR'] > 50 and i['ROIC'] > 30 and i['TOTAL_MARKET_CAP'] < 100000000000:  # 利润率高，增长10年
-        GROW_YEAR = 10
+        roic = 1
+        if i['ROIC']:
+            roic = i['ROIC']
+        # else:
+        #     print('NO roic in: ', i)
+        ROIC_RETURN = ROIC(roic, i['PBNEWMRQ'], GROWTHRATE)
+        # # 以长期投资回报率5%为分界, 计算合理估值
+        #
+        ROIC_PRICE = ROIC_RETURN / 5.0 * i['NEW_PRICE']
+        #
+        # # DCF
+        # # 以 净利润/ROIC/当前规模 判断公司的增长潜力, 高利润率，市值1000亿以下企业认为增长更久
+        CASH_PER = 1 + ((GROWTHRATE - 10) / 100)
+        GROW_YEAR = 5  # 利润率低，增长5年
+        if i['SALE_NPR'] > 50 and roic > 30 and i['TOTAL_MARKET_CAP'] < 10*Billion:  # 利润率高，增长10年
+            GROW_YEAR = 10
+        #
+        CASH_PRICE = DCF(i['PER_NETCASH_OPERATE'] * 1.35, GROW_YEAR, GROWTHRATE, 3, 10)
+        # SCORE = (CASH_PRICE / i['NEW_PRICE'] + ROIC_PRICE / i['NEW_PRICE']) / 2
+        SCORE = CASH_PRICE / i['NEW_PRICE']
+        PR = i['PE9']/roic
+        if ROIC_RETURN > 5 and PR < 2:
+        # if ROIC_RETURN > 2 and i['ROIC'] > 10 and PR < 1 and i['TOTAL_MARKET_CAP'] > 10000000000:
 
-    CASH_PRICE = DCF(i['PER_NETCASH_OPERATE'] * 1.35, GROW_YEAR, GROWTHRATE, 10)
-    SCORE = (CASH_PRICE / i['NEW_PRICE'] + ROIC_PRICE / i['NEW_PRICE']) / 2
-    PR = i['PE9']/i['ROIC']
-    # if ROIC_RETURN > 3 and PR < 2:
-    # if ROIC_RETURN > 2 and i['ROIC'] > 10 and PR < 1 and i['TOTAL_MARKET_CAP'] > 10000000000:
-    if ROIC_RETURN > 2 and i['ROIC'] > 20 and i['TOTAL_MARKET_CAP'] > 20000000000:
-        print([i['SECURITY_NAME_ABBR'], i['NEW_PRICE'], i['PBNEWMRQ'], i['PE9'], i['ROIC'], GROWTHRATE, ROIC_RETURN, ROIC_PRICE, CASH_PRICE, SCORE, PR])
+        # if i['PBNEWMRQ'] < 1:
+            # print('i:', i)
+            print([i['SECURITY_NAME_ABBR'], i['NEW_PRICE'], i['PBNEWMRQ'], i['PE9'], GROWTHRATE, CASH_PRICE, SCORE, roic])
+    except:
+        print("except:", i)
+
+# dcf_value = DCF(650, 5, 15, 3, 10)
+# print("DCF:", dcf_value)
